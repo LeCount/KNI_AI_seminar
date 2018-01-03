8975d64bfb17a8504c672d6a5cb214a68affeb52
stefan_home <stefan.danielsson.1989@gmail.com>
2018-01-03 20:03:35 +0100

    Added demos

---
+++ google_ai_challenge_ant_wars/README.txt.txt
@@ -0,0 +1,7 @@
+(1) Install Python
+(2) On windows - Run script: tools/play_one_game.cmd
+(3) On Linux - Run script: tools/./play_one_game.sh
+(4) Edit the script above to try new matches
+(5) Copy one of the bots and edit it
+
+More info here: http://ants.aichallenge.org/index.php
---
+++ google_ai_challenge_ant_wars/tools/play_one_game.cmd
@@ -0,0 +1,3 @@
+@echo off
+python "C:\Users\Greven\Downloads\tools\playgame.py" --engine_seed 42 --player_seed 42 --log_dir game_logs --turns 500 --map_file "C:\Users\Greven\Downloads\tools\maps\maze\maze_04p_01.map" %* "python ""C:\Users\Greven\Downloads\tools\sample_bots\python\GreedyBot.py""" "python ""C:\Users\Greven\Downloads\tools\sample_bots\python\GreedyBot.py""" "python ""C:\Users\Greven\Downloads\tools\sample_bots\python\GreedyBot.py""" "python ""C:\Users\Greven\Downloads\tools\sample_bots\python\GreedyBot.py"""
+
